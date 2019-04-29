90095c719521943e82ec19001034a3d57fcc7676
bigfang <bitair@gmail.com>
2019-04-29 16:38:06 +0800

    new repo add conflict check on --json --file

--- horn/impl/repo.py
+++ horn/impl/repo.py
@@ -14,12 +14,23 @@ def run(opts):
         'proj': Naming.camelize(opts.get('<folder>').split('/')[-1]),
         'file': opts.get('--file')
     }
-    bindings.update(json.loads(opts.get('--json')))
+    opt_json = json.loads(opts.get('--json'))
+    check_conflict(opt_json)
+    bindings.update(opt_json)
 
     if bindings.get('file'):
         with open(bindings.get('file')) as f:
             conf = json.load(f)
+            check_conflict(conf)
             bindings.update(conf)
 
     location = clone(bindings.get('repo'), bindings.get('checkout'))
     copy(f'{location}/new', bindings.get('folder'), data=bindings)
+
+
+def check_conflict(opt):
+    only_keys = ["folder", 'repo', 'checkout', 'file']
+    for ok in only_keys:
+        if ok in opt:
+            print(f'Conflict field found: {{{ok}: {opt.get(ok)}}}')
+            exit(1)
