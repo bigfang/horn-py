e52416b83802467bcbe078ae728c6b220dd0d247
bigfang <bitair@gmail.com>
2019-05-19 23:02:36 +0800

    refactor patterns

--- horn/cli/schema.py
+++ horn/cli/schema.py
@@ -73,8 +73,6 @@ def parse_fields(fields):
     attrs = [f.split(':') for f in fields]
     return [match(
         attr,
-        [_, _],               lambda x, y: {'field': x, 'type': validate_type(y, TYPES)},  # noqa: E241,E272
-        [_, 'nest', _],       lambda x, schema: {'field': x, 'type': 'Nested', 'schema': f'{inflection.camelize(schema)}Schema'},  # noqa: E241,E272
         [_, 'nest', _, TAIL], lambda x, schema, t: merge_fields({'field': x, 'type': 'Nested', 'schema': f'{inflection.camelize(schema)}Schema'}, validate_attr(t, AFFIXES, MOD_AFFIXES)),  # noqa: E241,E272
         [_, _, TAIL],         lambda x, y, t: merge_fields({'field': x, 'type': validate_type(y, TYPES)}, validate_attr(t, AFFIXES, MOD_AFFIXES))  # noqa: E241,E272
     ) for attr in attrs]
