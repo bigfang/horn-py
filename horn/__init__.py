04b974860073ad99fa47c69c698e6df89f1add1b
bigfang <bitair@gmail.com>
2023-07-22 19:10:59 +0800

    update ci

--- horn/__init__.py
+++ horn/__init__.py
@@ -44,7 +44,7 @@ from . import cli
 __all__ = ['main', '__version__']
 
 
-__version__ = '0.6.4'
+__version__ = '0.6.5'
 
 ACTION_MAP = {
     'new': ['<target>', '--app', '--proj', '--bare', '--pypi', '<from>', '<checkout>', '--json', '--file'],
